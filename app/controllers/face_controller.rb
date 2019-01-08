5e76db07d225be1e57e383f27782aec6590b15f8
trtum01 <ooavalanche@hotmail.com>
2019-01-08 13:23:29 +0700

    add upload button

--- app/controllers/face_controller.rb
+++ app/controllers/face_controller.rb
@@ -1,13 +1,14 @@
 class FaceController < ApplicationController
   def index
-      if params[:image_url]
-        @image_source = params[:image_url]
+      if params[:image_url] || params[:upload_path]
+        @image_source = params[:image_url] unless params[:image_url].blank?
+        @image_source_base = params[:upload_path] unless params[:upload_path].blank?
         if params[:coords_x]
           @headbuttx = params[:coords_x]
           @headbutty = params[:coords_y]
-          @face_size = face_size(@image_source)
+          @face_size = face_size(@image_source || @image_source_base)
 
-          @face_status = face_status_points(@image_source)
+          @face_status = face_status_points(@image_source || @image_source_base)
           @face_shape = drawing_face(@face_status)
           @result = analyzing_face(@face_shape, @headbuttx, @headbutty)
         end
