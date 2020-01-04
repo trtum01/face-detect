b8ac67d5e4b08f15328f3a820914851a3350b5cc
Tum <ooavalanche@hotmail.com>
2020-01-04 21:39:48 +0700

    -checking add rescue for checking when running in mobile

--- app/helpers/face_helper.rb
+++ app/helpers/face_helper.rb
@@ -6,69 +6,49 @@ module FaceHelper
         output_shape << position if position['id'].to_i <= 1040
       end
     rescue Exception
-      @error = face_detect_collection
+      @error << face_detect_collection
     end
     output_shape
   end
 
   def analyzing_face(face_shape, x, y)
-    calculate_line = []
-    wide_face = []
-    diff_degree = []
-    head = { "x" => x.to_i*100/@face_size['width'], "y" => y.to_i*100/@face_size['height'], "confidence" => 100, "id" => 1041}
-    long_face = []
-    long_face << head
+    begin
+      calculate_line = []
+      wide_face = []
+      diff_degree = []
+      head = { "x" => x.to_i*100/@face_size['width'], "y" => y.to_i*100/@face_size['height'], "confidence" => 100, "id" => 1041}
+      long_face = []
+      long_face << head
 
-    face_shape.each_cons(2) do |position|
-      calculate_line << slope_calculate(position[0]['x'], position[1]['x'], position[0]['y'], position[1]['y']).round(2) if position[1]['id'].to_i <= 1032
+      face_shape.each_cons(2) do |position|
+        calculate_line << slope_calculate(position[0]['x'], position[1]['x'], position[0]['y'], position[1]['y']).round(2) if position[1]['id'].to_i <= 1032
+      end
+
+      face_shape.each do |position|
+        wide_face << position if position['id'].to_i == 1024 || position['id'].to_i == 1040
+        long_face << position if position['id'].to_i == 1032
+      end
+
+      calculate_line.each_cons(2) do |degree|
+        diff_degree << (degree[0].abs - degree[1].abs).round.abs
+      end
+      format_shape(diff_degree,calculate_line,wide_face,long_face)
+    rescue Exception
+
     end
-
-    face_shape.each do |position|
-      wide_face << position if position['id'].to_i == 1024 || position['id'].to_i == 1040
-      long_face << position if position['id'].to_i == 1032
-    end
-
-    calculate_line.each_cons(2) do |degree|
-      diff_degree << (degree[0].abs - degree[1].abs).round.abs
-    end
-
-    format_shape(diff_degree,calculate_line,wide_face,long_face)
   end
 
   def format_shape(diff_degree,cal_line,wide,long)
-    wide_f = wide_face_calculate(wide, @face_size)
-    long_f = long_face_calculate(long, @face_size)
+    begin
+      wide_f = wide_face_calculate(wide, @face_size)
+      long_f = long_face_calculate(long, @face_size)
 
-    if diff_degree[3] <= diff_degree[6]
-      format_face = "triangle"
-      if diff_degree[6] - diff_degree[3] >= 8
+      if diff_degree[3] <= diff_degree[6]
         format_face = "triangle"
-      elsif (diff_degree[2] - diff_degree[4]).abs <= 4
-        if (diff_degree[1] - diff_degree[5]).abs <= 4
-          if wide_f/long_f < 0.725
-            format_face = "oval"
-          else
-            format_face = "circle"
-          end
-        end
-      end
-    elsif diff_degree[6] - diff_degree[3] >= 8
-      format_face = "triangle"
-    elsif (diff_degree[2] - diff_degree[4]).abs <= 4 || (diff_degree[1] - diff_degree[5]).abs <= 4
-      if wide_f/long_f < 0.725
-        format_face = "oval"
-      else
-        format_face = "circle"
-      end
-    elsif diff_degree[3] >= 20
-      if wide_f/long_f < 0.725
-        format_face = "rectangle"
-      else
-        format_face = "square"
-      end
-      if cal_line[0] > 80.5
-        if (diff_degree[3] - diff_degree[6] > 4)
-          if (diff_degree[2] - diff_degree[4]).abs <= 4 && ​if(diff_degree[1] - diff_degree[5]).abs <= 4
+        if diff_degree[6] - diff_degree[3] >= 8
+          format_face = "triangle"
+        elsif (diff_degree[2] - diff_degree[4]).abs <= 4
+          if (diff_degree[1] - diff_degree[5]).abs <= 4
             if wide_f/long_f < 0.725
               format_face = "oval"
             else
@@ -76,26 +56,52 @@ module FaceHelper
             end
           end
         end
-      end
-      if diff_degree[3] - diff_degree[6] >= 8
-        if (diff_degree[2] - diff_degree[4]).abs <= 4 || (diff_degree[1] - diff_degree[5]).abs <= 4
-          if wide_f/long_f < 0.725
-            format_face = "oval"
-          else
-            format_face = "circle"
+      elsif diff_degree[6] - diff_degree[3] >= 8
+        format_face = "triangle"
+      elsif (diff_degree[2] - diff_degree[4]).abs <= 4 || (diff_degree[1] - diff_degree[5]).abs <= 4
+        if wide_f/long_f < 0.725
+          format_face = "oval"
+        else
+          format_face = "circle"
+        end
+      elsif diff_degree[3] >= 20
+        if wide_f/long_f < 0.725
+          format_face = "rectangle"
+        else
+          format_face = "square"
+        end
+        if cal_line[0] > 80.5
+          if (diff_degree[3] - diff_degree[6] > 4)
+            if (diff_degree[2] - diff_degree[4]).abs <= 4 && ​if(diff_degree[1] - diff_degree[5]).abs <= 4
+              if wide_f/long_f < 0.725
+                format_face = "oval"
+              else
+                format_face = "circle"
+              end
+            end
           end
         end
-      end
-    elsif (diff_degree[2] - diff_degree[4]).abs <= 4 || (diff_degree[1] - diff_degree[5]).abs <= 4
-      if wide_f/long_f < 0.725
-        format_face = "oval"
+        if diff_degree[3] - diff_degree[6] >= 8
+          if (diff_degree[2] - diff_degree[4]).abs <= 4 || (diff_degree[1] - diff_degree[5]).abs <= 4
+            if wide_f/long_f < 0.725
+              format_face = "oval"
+            else
+              format_face = "circle"
+            end
+          end
+        end
+      elsif (diff_degree[2] - diff_degree[4]).abs <= 4 || (diff_degree[1] - diff_degree[5]).abs <= 4
+        if wide_f/long_f < 0.725
+          format_face = "oval"
+        else
+          format_face = "circle"
+        end
       else
-        format_face = "circle"
+        format_face = ""
       end
-    else
-      format_face = ""
-    end
+      information_shape(format_face)
+    rescue Exception
 
-    information_shape(format_face)
+    end
   end
 end
